0a798492670107449cc0e2e89a17447dd432832e
FaiziCodeSpace <faizanwebdev1@gmail.com>
2025-09-07 23:46:12 +0500

    feat(signals): implement counter and dynamic input using signals

--- src/app/signals/signals.ts
+++ src/app/signals/signals.ts
@@ -2,30 +2,30 @@ import { Component, effect, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
-  imports: [],
+  standalone: true,   // ✅ since you used imports: [], make it standalone
   templateUrl: './signals.html',
-  styleUrl: './signals.css'
+  styleUrls: ['./signals.css'] // ✅ should be plural "styleUrls"
 })
 export class Signals {
   value: WritableSignal<number | string> = signal(0);
-  count= signal<number>(0);
+  count = signal<number>(0);
   number: WritableSignal<number> = signal(0);
-  constructor(){
+
+  constructor() {
     effect(() => {
       console.log('Number changed:', this.number());
     });
   }
-  clickHandle(condition:string) {
-    if(condition=="increase"){
-    this.number.set(this.number()+1);
-    }else if(condition=="decrease"){
-      this.number()>=1?this.number.set(this.number()-1):null;
+
+  clickHandle(condition: string) {
+    if (condition === 'increase') {
+      this.number.set(this.number() + 1);
+    } else if (condition === 'decrease') {
+      this.number() >= 1 ? this.number.set(this.number() - 1) : null;
     }
   }
+
   valCall(val: any) {
     this.value.set(val);
   }
-
-
-
 }
